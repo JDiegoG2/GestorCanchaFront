256d6c3bb95c29d49b53eff6a781fb8d3ca939e1
Carlos Salas <csalastoledo@gmail.com>
2024-07-31 21:46:19 -0500

    configuration initial models and auth.service login and register

---
+++ src/app/models/register.model.ts
@@ -0,0 +1,10 @@
+// src/app/models/register.model.ts
+export interface Register {
+  username: string;
+  password: string;
+  nro_documento: string;
+  nombres: string;
+  apellidos: string;
+  telefono: string;
+  email: string;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Register } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/login';
+  private apiUrl = 'http://localhost:9000/';  // URL base de tu API
 
   constructor(private http: HttpClient) {}
 
+  // Método para el login
   login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(this.apiUrl, { username, password });
+    const url = `${this.apiUrl}login`;
+    return this.http.post<any>(url, { username, password });
+  }
+
+  // Método para el registro
+  register(user: Register): Observable<any> {
+    const url = `${this.apiUrl}register`;
+    return this.http.post<any>(url, user);
   }
 }
