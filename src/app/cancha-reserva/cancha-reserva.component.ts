9325c4319e7896e7bc98b3fbb1a8e571cb029965
Johan Gatica <johan.g.arevalo@gmail.com>
2024-08-17 12:52:53 -0500

    Fix cancha reserva html and component, fix cancha service

--- src/app/cancha-reserva/cancha-reserva.component.ts
+++ src/app/cancha-reserva/cancha-reserva.component.ts
@@ -5,7 +5,6 @@ import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { CanchaService } from '../services/cancha.service';
 import { Cancha } from '../models/cancha.model';
-import { SedeComponent } from '../sede/sede.component';
 import { SedeService } from '../services/sede.service';
 import { Sede } from '../models/sede.model';
 import { TipoCanchaService } from '../services/tipo-cancha.service';
@@ -13,7 +12,7 @@ import { TipoCanchaService } from '../services/tipo-cancha.service';
 @Component({
   selector: 'app-cancha-reserva',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, SedeComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cancha-reserva.component.html',
   styleUrls: ['./cancha-reserva.component.css']
 })
@@ -22,8 +21,9 @@ export class CanchaReservaComponent implements OnInit {
   canchaForm: FormGroup;
   canchaAPI: Cancha[] = [];
   sedes: Sede[] = [];
+  sedesActivas: Sede[] = [];
   tiposCancha: string[] = [];
-
+  editing: boolean = false; // Flag para determinar si estamos editando
 
   constructor(
     private fb: FormBuilder,
@@ -36,11 +36,11 @@ export class CanchaReservaComponent implements OnInit {
     this.canchaForm = this.fb.group({
       id: [0],
       tipo_cancha: ['', Validators.required],
-      numero: ['', Validators.required],
-      precio: [0, Validators.required],
-      sede_id: [undefined, Validators.required],
-      dis_hr_inicio: [0, Validators.required],
-      dis_hr_fin: [24, Validators.required]
+      numero: ['', [Validators.required, Validators.pattern(/^[0-9]+$/), Validators.min(0)]], // Solo números positivos
+      precio: [0, [Validators.required, Validators.min(0)]], // Precio no negativo
+      sede_id: [0, Validators.required],
+      dis_hr_inicio: [0, [Validators.required, Validators.min(0), Validators.max(23)]], // Horas de 0 a 23
+      dis_hr_fin: [0, [Validators.required, Validators.min(0), Validators.max(23)]]
     });
   }
 
@@ -48,13 +48,12 @@ export class CanchaReservaComponent implements OnInit {
     this.loadCanchas();
     this.loadSedes();
     this.loadTiposCancha();
-
-
+    this.cargarSedesActivas();
   }
 
   private loadSedes(): void {
     this.sedeService.listarSedes().subscribe(
-      (data: any[]) => {
+      (data: Sede[]) => {
         console.log('Sedes cargadas:', data); // Log para depurar
         this.sedes = data;
       },
@@ -62,6 +61,17 @@ export class CanchaReservaComponent implements OnInit {
         console.error('Error al cargar las sedes:', error);
       }
     );
+  }
+
+  cargarSedesActivas(): void {
+    this.sedeService.listarSedesActivas().subscribe({
+      next: (data) => {
+        this.sedesActivas = data;
+      },
+      error: (err) => {
+        console.error('Error al cargar sedes activas: ', err);
+      }
+    });
   }
 
   private loadTiposCancha(): void {
@@ -76,40 +86,68 @@ export class CanchaReservaComponent implements OnInit {
     );
   }
 
-  toggleEstado(cancha: Cancha): void {
-    const nuevoEstado = !cancha.estado;
-    this.canchaService.toggleEstadoCancha(cancha.id, nuevoEstado).subscribe(
+  toggleEstado(cancha: Cancha) {
+    const nuevoEstado = !cancha.estado; // Cambia el estado al opuesto
+    this.canchaService.actualizarEstado(cancha.id, nuevoEstado).subscribe(
       response => {
-        cancha.estado = nuevoEstado;
-        this.toastr.success('Estado de la cancha actualizado correctamente.');
+        console.log('Estado actualizado', response);
+        cancha.estado = nuevoEstado; // Actualiza el objeto en la lista
       },
       error => {
-        console.error('Error al cambiar el estado', error);
-        this.toastr.error('Error al cambiar el estado de la cancha');
+        console.error('Error al actualizar el estado', error);
       }
     );
   }
 
+  editCancha(cancha: Cancha): void {
+    this.editing = true;
+    this.canchaForm.setValue({
+      id: cancha.id,
+      tipo_cancha: cancha.tipo_cancha,
+      numero: cancha.numero,
+      precio: cancha.precio,
+      sede_id: cancha.sede_id, // Asegúrate de usar el ID correcto
+      dis_hr_inicio: cancha.dis_hr_inicio,
+      dis_hr_fin: cancha.dis_hr_fin
+      // No incluyas `estado` si no es necesario en el formulario
+    });
+  }
+
   onSubmit(): void {
     if (this.canchaForm.valid) {
-      const newCancha = this.canchaForm.value as Cancha;
-      newCancha.estado = true;
+      const formValue = this.canchaForm.value as Cancha;
 
-      this.canchaService.guardarCancha(newCancha).subscribe({
-        next: (resp) => {
-          this.canchaAPI.push(resp);
-          this.toastr.success('Cancha agregada correctamente.');
-          this.canchaForm.reset();
-        },
-        error: (err) => {
-          console.error('Error al crear la cancha: ', err);
-          if (err !== undefined) {
-            this.toastr.error(err.error.mensaje);
-          } else {
-            this.toastr.error('Error al crear la cancha');
+      if (this.editing) {
+        this.canchaService.actualizarCancha(formValue.id, formValue).subscribe({
+          next: (resp) => {
+            this.loadCanchas(); // Recargar la lista de canchas
+            this.toastr.success('Cancha actualizada correctamente.');
+            this.canchaForm.reset();
+            this.editing = false;
+          },
+          error: (err) => {
+            console.error('Error al actualizar la cancha: ', err);
+            this.toastr.error('Error al actualizar la cancha');
           }
-        }
-      });
+        });
+      } else {
+        const formValue = this.canchaForm.value as Cancha;
+        this.canchaService.guardarCancha(formValue).subscribe({
+          next: (resp) => {
+            this.canchaAPI.push(resp);
+            this.toastr.success('Cancha agregada correctamente.');
+            this.canchaForm.reset();
+          },
+          error: (err) => {
+            console.error('Error al crear la cancha: ', err);
+            if (err !== undefined) {
+              this.toastr.error(err.error.mensaje);
+            } else {
+              this.toastr.error('Error al crear la cancha');
+            }
+          }
+        });
+      }
     } else {
       this.toastr.warning('Por favor, complete todos los campos requeridos.');
     }
@@ -118,6 +156,14 @@ export class CanchaReservaComponent implements OnInit {
   loadCanchas(): void {
     this.canchaService.listarCanchas().subscribe({
       next: (data) => {
+        // Log para verificar los datos recibidos
+        console.log('Datos recibidos:', data);
+
+        // Verifica el estado de cada cancha
+        data.forEach(cancha => {
+          console.log(`Cancha ID: ${cancha.id}, Estado: ${cancha.estado}`);
+        });
+
         this.canchaAPI = data;
         console.log('Lista de canchas actualizada.');
       },
@@ -127,6 +173,8 @@ export class CanchaReservaComponent implements OnInit {
       }
     });
   }
+
+
 
   trackById(index: number, item: Cancha): any {
     return item.id;
--- src/app/services/cancha.service.ts
+++ src/app/services/cancha.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cancha } from '../models/cancha.model';
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service';  // Importa el servicio de autenticación
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +10,20 @@ import { AuthService } from './auth.service';
 export class CanchaService {
   private apiUrl = 'http://localhost:9000/cancha';
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   private getHeaders(): HttpHeaders {
     const token = this.authService.getToken();
     return new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${token}`  // Incluye el token en los encabezados
     });
   }
 
   // Método para actualizar el estado de una cancha
-  toggleEstadoCancha(id: number, nuevoEstado: boolean): Observable<Cancha> {
-    return this.http.patch<Cancha>(`${this.apiUrl}/estado/${id}`, { estado: nuevoEstado }, { headers: this.getHeaders() });
+  actualizarEstado(id: number, estado: boolean): Observable<any> {
+    // Convierte el estado booleano a 0 o 1
+    const estadoValue = estado ? 1 : 0;
+    return this.http.patch(`${this.apiUrl}/estado/${id}`, { estado: estadoValue }, {headers: this.getHeaders()});
   }
 
   guardarCancha(cancha: Cancha): Observable<Cancha> {
