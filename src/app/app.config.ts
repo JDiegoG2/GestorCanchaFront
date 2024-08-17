f4d5d59ebfa66be9b777f0d415c12bcb5fdd575f
Johan Gatica <johan.g.arevalo@gmail.com>
2024-08-17 12:47:22 -0500

    Fix routes and app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, FormsModule),
+    importProvidersFrom(HttpClientModule, FormsModule,ReactiveFormsModule),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CanchaReservaComponent } from './cancha-reserva/cancha-reserva.component';
 import { SedeComponent } from './sede/sede.component';
+import { ReservaComponent } from './reserva/reserva.component';
+  // Asegúrate de que este componente esté importado
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirige a la página de login por defecto
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'cancha-reserva', component: CanchaReservaComponent },
-  {path: 'sede', component: SedeComponent}
+  { path: 'sede', component: SedeComponent },
+  { path: 'reservar', component: ReservaComponent },  // Ruta para el componente de reservas
+  { path: '**', redirectTo: 'login' }  // Ruta comodín para manejar rutas no existentes
 ];
