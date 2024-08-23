27c59592ebe300d261e65bce912bc09fc75eea77
glmy07 <I202224598@cibertec.edu.pe>
2024-08-23 00:30:02 -0500

    fix model

--- src/app/models/reserva-response.model.ts
+++ src/app/models/reserva-response.model.ts
@@ -1,13 +1,13 @@
-import { Cancha } from "./cancha.model";
 
 export interface ReservaResponse {
   id: number;
   fechaCreacion: string;    // Falta la fecha de creación
-  fechaReserva: string;
+  fecha_reserva: string;
   hora_reserva: number;
   cancha: string;           // Puede ser una cadena que combine número de cancha y tipo
   observacion: string;      // Falta la observación
   importe: number;          // Falta el importe
   estado: string;           // Falta el estado de la reserva
   cliente: string;          // Falta el cliente que realizó la reserva
+  sede: string;
 }
