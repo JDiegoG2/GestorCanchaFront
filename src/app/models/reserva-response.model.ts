01964fa3083cdc5ce92327269bb8cf90599b05e5
Johan Gatica <diego.gatica.arevalo@gmail.com>
2024-08-23 00:00:03 -0500

    fix model

--- src/app/models/reserva-response.model.ts
+++ src/app/models/reserva-response.model.ts
@@ -4,7 +4,7 @@ export interface ReservaResponse {
   id: number;
   fechaCreacion: string;    // Falta la fecha de creación
   fechaReserva: string;
-  horaReserva: number;
+  hora_reserva: number;
   cancha: string;           // Puede ser una cadena que combine número de cancha y tipo
   observacion: string;      // Falta la observación
   importe: number;          // Falta el importe
